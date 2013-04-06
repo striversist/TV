bd5e0503429db8abff8d67e00c7038c1cdeca89d
striversist <striversist@qq.com>
2013-04-06 11:33:16 +0800

    1. 增加xml/channels.xml 2. 在ProgramFilter中增加对特殊格式的处理(如test/beijingst.htm test/dongfangst.htm)

--- ProgramFilter.php
+++ ProgramFilter.php
@@ -1,6 +1,7 @@
 <?php
 require_once (dirname(__FILE__).'/'.'../simplehtmldom_1_5/simple_html_dom.php');
 
+set_time_limit(0);
 class ProgramFilter
 {
     public static function getInstance()
@@ -22,24 +23,39 @@ class ProgramFilter
             if(!count($titles))  // 过滤“广告”
             {
                 $titles = $pmt1->find("div[id=e2]");
+                
+                // Deal with special case: <div id="e2">abcd<a href="xxx">...</a></div>
+                $start = strpos($titles[0]->outertext, ">", 1);
+                $end = strpos($titles[0]->outertext, "<", 1);
+                $substr = substr($titles[0]->outertext, $start + 1, $end - $start - 1);
+                $titles[0]->plaintext = $substr;
+                
                 if(!count($titles))
                     continue;
             }
-            $list[] = array("time" => $times[0]->plaintext, "title" => $titles[0]->plaintext);
+            //echo "time: ".$times[0]->plaintext."  ";
+            //echo "title: ".$titles[0]->plaintext."<br />";
+            $list[] = @array("time" => $times[0]->plaintext, "title" => $titles[0]->plaintext);
         }
         foreach ($html->find("div[id=PMT2]") as $pmt2)
         {
             $times = $pmt2->find("div[id=e1] b font");
             $titles = $pmt2->find("div[id=e2] a.black");
+            $substr = null;
             if(!count($titles))  // 过滤“广告”
             {
                 $titles = $pmt2->find("div[id=e2]");
+                
+                // Deal with special case: <div id="e2">abcd<a href="xxx">...</a></div>
+                $start = strpos($titles[0]->outertext, ">", 1);
+                $end = strpos($titles[0]->outertext, "<", 1);
+                $substr = substr($titles[0]->outertext, $start + 1, $end - $start - 1);
+                $titles[0]->plaintext = $substr;
+                
                 if(!count($titles))
                     continue;
             }
-            //$text_time = iconv("GB2312", "UTF-8", $times[0]->plaintext);
-            //$text_title = iconv("GB2312", "UTF-8", $titles[0]->plaintext);
-            //$list[] = array("time" => $text_time, "title" => $text_title);
+
             $list[] = @array("time" => $times[0]->plaintext, "title" => $titles[0]->plaintext);
         }
         

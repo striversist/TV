33f2c89fcfd7247b98341569354589bd31c8700f
striversist <striversist@qq.com>
2013-05-26 23:22:50 +0800

    测试：修正网页部分字符乱码（未解决）

--- testUtf8.php
+++ testUtf8.php
@@ -3,14 +3,24 @@
     require_once dirname(__FILE__).'/'.'./app/utils.php';
     
     $url = "http://localhost/projects/TV/test.html";
+    //$url = "http://localhost/projects/TV/test.txt";
     $html = file_get_contents($url);
-    //$pos = strpos($html, "甄嬛传");
-    $pos = strpos($html, "\x53\x71");
+    //echo"orign html:"."<br />".$html."<br />";
+    $src_text = "\xD5\xE7\x8B\xD6\xB4\xAB\r";     // gbk/gb2312: 甄嬛传
+    $pos = strpos($html, $src_text);
     var_dump($pos);
     
-    $html = gb2312_to_utf8($html);;
-    echo $html;
+    //$html = str_replace($src_text, "aaaaaaaaa", $html);
+    $html = gb2312_to_utf8($html);
+    echo $html."<br />";
     
-    $pos = strpos($html, "甄");
-    var_dump($pos)
+    echo "<br /><br />";
+    $converted = gb2312_to_utf8($src_text);
+    echo $converted;
+    
+    $pos = strpos($html, $converted);
+    var_dump($pos);
+    
+    $html = str_replace($converted, "甄嬛传", $html);
+    echo $html;
 ?>

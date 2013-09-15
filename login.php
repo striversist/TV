edee03732717ca4e957ec0a23d45dfe8e34b95fd
striversist <striversist@qq.com>
2013-09-15 19:56:32 +0800

    【功能】在login中记录下用户的UserAgent

--- login.php
+++ login.php
@@ -7,11 +7,14 @@
     if (!isset($headers["GUID"]))
         return;
     $guid = $headers["GUID"];
+    @$ua = $headers["UA"];
     $profile = $db->getProfile($guid);
     if ($profile == false)
         return;
     
     $date = date("Y/m/d H:i:s");
     $profile["LastLogin"] = $date;
+    if ($ua != null)
+        $profile["UA"] = $ua;
     $db->storeProfile($profile);
 ?>

896b45ba52d2cb565fc1d922919f428873ee4244
striversist <striversist@qq.com>
2013-09-19 23:25:49 +0800

    【功能】增加频道版本信息，用于终端判断是否需要重新拉去列表。若修改了频道信息，应将该版本加1.

--- public/update/update.php
+++ public/update/update.php
@@ -1,5 +1,6 @@
 <updateinfo>
-	<versionCode>2</versionCode>
-	<versionName>1.0.2</versionName>
-	<url>http://192.168.1.104/projects/TV/update/apk/TVGuide_1.0.2.apk</url>
+    <versionCode>2</versionCode>
+    <versionName>1.0.2</versionName>
+    <url>http://192.168.1.104/projects/TV/update/apk/TVGuide_1.0.2.apk</url>
+    <channelVersion>1</channelVersion>
 </updateinfo>

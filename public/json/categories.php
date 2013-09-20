02fd8f44064906de6ddb69f338e5c399e407c0b9
striversist <striversist@qq.com>
2013-09-20 08:59:14 +0800

    【功能】将首次分配GUID的逻辑从categories.php移动到login.php中，更符合逻辑。这样首次分配时的profile也能有LastLogin的信息了。

--- public/json/categories.php
+++ public/json/categories.php
@@ -1,7 +1,6 @@
 <?php
     header("Content-type: text/html; charset=utf8");
     require_once dirname(__FILE__).'/'.'../../Collector.php';
-    require_once dirname(__FILE__).'/'.'../../app/utils.php';
     require_once dirname(__FILE__).'/'.'../../Database.php';
     
     $type = null;
@@ -10,20 +9,7 @@
     {
         $type = $_GET["type"];
     }
-    
-    // Detect GUID header, is not set, generate a GUID header
-    $headers = apache_request_headers();
-    if (!isset($headers["GUID"]))
-    {
-        $guid = guid();
-        header("GUID: ".$guid);
-        $db = Database::getInstance();
-        $profile["first_use"] = date("Y/m/d H:i:s");
-        $profile["GUID"] = $guid;
-        $db->storeProfile($profile);
-//        var_dump($profiles);
-    }
-    
+        
     $colletor = Collector::getInstance();
     
     if ($type == null or $type == "root")  // Default: get root categories
--- public/login.php
+++ public/login.php
@@ -1,17 +1,28 @@
 <?php
     header("Content-type: text/html; charset=utf8");
     require_once dirname(__FILE__).'/'.'../Database.php';
+    require_once dirname(__FILE__).'/'.'../app/utils.php';
     
-    $db = Database::getInstance();
+    // Detect GUID header, is not set, generate a GUID header
     $headers = apache_request_headers();
+    $db = Database::getInstance();
+    $profile = array();
     if (!isset($headers["GUID"]))
-        return;
-    $guid = $headers["GUID"];
-    @$ua = $headers["UA"];
-    $profile = $db->getProfile($guid);
-    if ($profile == false)
-        return;
+    {
+        $guid = guid();
+        header("GUID: ".$guid);
+        $profile["GUID"] = $guid;
+        $profile["first_use"] = date("Y/m/d H:i:s");
+    }
+    else
+    {
+        $guid = $headers["GUID"];
+        $profile = $db->getProfile($guid);
+        if ($profile == false)
+            return;
+    }
     
+    @$ua = $headers["UA"];  
     $date = date("Y/m/d H:i:s");
     $profile["LastLogin"] = $date;
     if ($ua != null)

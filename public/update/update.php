0a4f0c474969f743f39bb2b5e6d4664d43abe3a7
striversist <striversist@qq.com>
2013-09-20 09:41:43 +0800

    【功能】更新url中加上端口号

--- public/update/update.php
+++ public/update/update.php
@@ -5,7 +5,7 @@ echo <<< EOT
 <Updateinfo>
     <VersionCode>2</VersionCode>
     <VersionName>1.0.2</VersionName>
-    <Url>http://striversist.oicp.net/projects/TV/public/update/apk/TVGuide_1.0.2.apk</Url>
+    <Url>http://striversist.oicp.net:9999/projects/TV/public/update/apk/TVGuide_1.0.2.apk</Url>
     <ChannelVersion>1</ChannelVersion>
 </Updateinfo>
 EOT;

c6a9df329bdb664c14972b26bf29d432963e2f1e
striversist <striversist@qq.com>
2013-09-21 23:33:41 +0800

    【功能】根据用户地点判断搜索结果中是否包含外地的节目信息。如武汉的用户搜索不到北京本地的频道内容，但可以搜出北京卫视的内容。（因为卫视频道为全国性的频道）

--- public/json/search.php
+++ public/json/search.php
@@ -15,6 +15,13 @@
     $db = Database::getInstance();
     $colletor = Collector::getInstance();
     $channels = $db->getChannels();
+    $profile = false;
+    $headers = apache_request_headers();
+    if (isset($headers["GUID"]))
+    {
+        $guid = $headers["GUID"];
+        $profile = $db->getProfile($guid);
+    }  
     
     $today = date("w");
     if ($today == "0")    // Sunday
@@ -26,6 +33,8 @@
     $result = array();
     foreach ($channels as $id => $channel)
     {
+        if (!needSearch($profile, $channel))
+            continue;
         foreach ($channel["days"] as $day => $programs) 
         {
             if ($day == $today)
@@ -75,11 +84,8 @@
     }
     
     // 收集用户搜索记录
-    $headers = apache_request_headers();
     if (!isset($headers["GUID"]))
         return;
-    $guid = $headers["GUID"];
-    $profile = $db->getProfile($guid);
     if ($profile == false)
         return;
     
@@ -98,4 +104,52 @@
     }
     $profile["SearchRecords"] = $search_records;
     $db->storeProfile($profile);
+    
+    // ------------------------- Functions -----------------------------------
+    function needSearch($profile, $channel)
+    {
+        // 传入的无效信息，无法判断，统一需要search
+        if ($profile == false || $channel == false)
+            return true;
+        // 若不在local类别中，则为全国频道，需要search
+        if (!isInOnlyLocalCategory($channel))
+            return true;
+        // 若在local类别中，则需匹配用户所在位置，匹配则需要search
+        if (isMatchUserLocation($profile, $channel))
+            return true;
+        return false;
+    }
+    
+    function isInOnlyLocalCategory($channel)
+    {
+        // 若节目的类别属性中有local，则为属于本地节目
+        $count = count(@$channel["categories"]);
+        foreach (@$channel["categories"] as $category_id => $category_name)
+        {
+            // 若含有local类别，且该节目具有的类别总数为2
+            // （一个local，一个具体的city或province），则该节目只属于本地节目
+            if ($category_id == "local" and $count == 2)
+                return true;
+        }
+        return false;
+    }
+    
+    function isMatchUserLocation($profile, $channel)
+    {
+        if (!isset($profile["UL"]))
+            return false;
+        
+        $location = $profile["UL"];
+        foreach ($channel["categories"] as $category_id => $category_name)
+        {
+            // 若发现本地类型类型名称“北京、武汉...”出现在$location中，
+            // 则说明用户所在地的本地类型为“北京、武汉...”
+            if (strpos($location, $category_name) !== FALSE)
+            {
+//                echo "Found $location contains $category_name"."<br />";
+                return true;
+            }
+        }
+        return false;
+    }
 ?>

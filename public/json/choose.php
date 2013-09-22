3dc00aac0522384237abd0093aecefbf3354ea5d
striversist <striversist@qq.com>
2013-09-22 21:54:33 +0800

    【功能】【重构】增加在查询channel的节目信息时，返回正在播放的节目功能。只要GET中带上onplaying或者onplaying=1

--- public/json/choose.php
+++ public/json/choose.php
@@ -15,18 +15,55 @@
         echo json_encode($result);
         return;
     }
+    $needOnPlaying = false;
+    if (isset($_GET["onplaying"]))
+        $needOnPlaying = true;
     $db = Database::getInstance();
     $channels = $db->getChannels();
+    
     foreach ($channels[$id]["days"][$day] as $program)
     {
         //echo $program["time"].": ".$program["title"]."<br />";
         $array[] = array("time" => $program["time"], "title" => $program["title"]);
     }
+    
     $result["result"] = $array;
     $result["id"] = $id;
     $result["day"] = $day;
+    if ($needOnPlaying)
+    {
+        $program = getOnPlayingProgram($channels[$id]);
+        $result["onplaying"] = $program;
+    }
+    
     echo json_encode($result);
     //dump($channels);
+    
+    // --------------------------- Functions --------------------------------
+    function getOnPlayingProgram($channel)
+    {
+        $program = array();
+        $today = date("w");
+        if ($today == "0")    // Sunday
+            $today = "7";
+        $now = date("H:i");
+
+        //echo "id=$id, today=$today, now=$now"."<br />";
+        @$programs = $channel["days"][$today];        // id可能为未知，用@抑制错误
+
+        for($i=0; $i<count($programs); $i++)
+        {
+            // TODO: 跨天的情况没有考虑，不过考虑转钟时使用的人数非常少，放在将来实现该功能
+            if (($now >= $programs[$i]["time"] && $now < @$programs[$i+1]["time"]) or ($i == count($programs) - 1))
+            {
+    //            echo "Found the program now playing: ".$programs[$i]['time'].": ".$programs[$i]['title']."<br />";
+                $program["time"] = $programs[$i]['time'];
+                $program["title"] = $programs[$i]['title'];
+                break;
+            }
+        }
+        return $program;
+    }
     
     function getNameOfDay($day)
     {   
--- public/json/onplaying_program.php
+++ public/json/onplaying_program.php
@@ -10,28 +10,37 @@
     $id = htmlspecialchars($_GET["channel"]);
     $db = Database::getInstance();
     $channels = $db->getChannels();
-    $today = date("w");
-    if ($today == "0")    // Sunday
-    {
-        $today = "7";
-    }
-    $now = date("H:i");
-    $result = array("id" => $id);
-    
-    //echo "id=$id, today=$today, now=$now"."<br />";
-    @$programs = $channels[$id]["days"][$today];        // id可能为未知，用@抑制错误
-
-    for($i=0; $i<count($programs); $i++)
-    {
-        // TODO: 跨天的情况没有考虑，不过考虑转钟时使用的人数非常少，放在将在实现该功能
-        if (($now >= $programs[$i]["time"] && $now < @$programs[$i+1]["time"]) or ($i == count($programs) - 1))
-        {
-//            echo "Found the program now playing: ".$programs[$i]['time'].": ".$programs[$i]['title']."<br />";
-            $result["time"] = $programs[$i]['time'];
-            $result["title"] = $programs[$i]['title'];
-            break;
-        }
-    }
+    $result = array();
+    $program = getOnPlayingProgram($channels[$id]);
+    $result["id"] = $id;
+    $result["time"] = $program["time"];
+    $result["title"] = $program["title"];
     
     echo json_encode($result);
+    
+    // ------------------------- Functions ---------------------------------
+    function getOnPlayingProgram($channel)
+    {
+        $program = array();
+        $today = date("w");
+        if ($today == "0")    // Sunday
+            $today = "7";
+        $now = date("H:i");
+
+        //echo "id=$id, today=$today, now=$now"."<br />";
+        @$programs = $channel["days"][$today];        // id可能为未知，用@抑制错误
+
+        for($i=0; $i<count($programs); $i++)
+        {
+            // TODO: 跨天的情况没有考虑，不过考虑转钟时使用的人数非常少，放在将来实现该功能
+            if (($now >= $programs[$i]["time"] && $now < @$programs[$i+1]["time"]) or ($i == count($programs) - 1))
+            {
+    //            echo "Found the program now playing: ".$programs[$i]['time'].": ".$programs[$i]['title']."<br />";
+                $program["time"] = $programs[$i]['time'];
+                $program["title"] = $programs[$i]['title'];
+                break;
+            }
+        }
+        return $program;
+    }
 ?>

56b9d5d422b2e90aae5c559ea008df8d29982ff5
striversist <striversist@qq.com>
2013-10-04 22:14:37 +0800

    【重构】删除无用代码

--- public/json/search.php
+++ public/json/search.php
@@ -27,7 +27,6 @@
         $today = "7";
     
     // 匹配关键字
-    // 方法1（优化过）：
     $result = array();
     $search_categories = getSearchCategories($profile);
     foreach ($search_categories as $category_id)
@@ -58,34 +57,6 @@
             }
         }
     }
-    
-    // 方法2：
-//    $channels = $db->getChannels();
-//    $result = array();
-//    foreach ($channels as $id => $channel)
-//    {
-//        if (!needSearch($profile, $channel))
-//            continue;
-//        foreach ($channel["days"] as $day => $programs) 
-//        {
-//            if ($day == $today)
-//            {
-//                $tmp = array();
-//                foreach ($programs as $program)
-//                {
-//                    if (strpos($program["title"], $keyword) !== FALSE)
-//                    {
-//                        //echo "You found $keyword in ".$program["title"]."<br />";
-//                        $tmp[] = $program;
-//                    }
-//                }
-//                if (count($tmp))
-//                {
-//                    $result["$id"] = $tmp;
-//                }
-//            }
-//        }
-//    }
     
     // 返回用户结果
     if (count($result))
@@ -137,66 +108,6 @@
     $db->storeProfile($profile);
     
     // ------------------------- Functions -----------------------------------
-//    function needSearch($profile, $channel)
-//    {
-//        // 传入的无效信息，无法判断，统一需要search
-//        if ($profile == false || $channel == false)
-//            return true;
-//        // 若不在local类别中，则为全国频道，需要search
-//        if (!isInOnlyLocalCategory($channel))
-//            return true;
-//        // 若在local类别中，则需匹配用户所在位置，匹配则需要search
-//        if (isMatchUserLocation($profile, $channel))
-//            return true;
-//        return false;
-//    }
-//    
-//    function isInOnlyLocalCategory($channel)
-//    {
-//        // 若节目的类别属性中有local，则为属于本地节目
-//        $count = count(@$channel["categories"]);
-//        foreach (@$channel["categories"] as $category_id => $category_name)
-//        {
-//            // 若含有local类别，且该节目具有的类别总数为2
-//            // （一个local，一个具体的city或province），则该节目只属于本地节目
-//            if ($category_id == "local" and $count == 2)
-//                return true;
-//        }
-//        return false;
-//    }
-//    
-//    function isMatchUserLocation($profile, $channel)
-//    {
-//        if (!isset($profile["UL"]))
-//            return false;
-//        
-//        $location = $profile["UL"];
-//        foreach ($channel["categories"] as $category_id => $category_name)
-//        {
-//            // 若发现本地类型类型名称“北京、武汉...”出现在$location中，
-//            // 则说明用户所在地的本地类型为“北京、武汉...”
-//            if (strpos($location, $category_name) !== FALSE)
-//            {
-////                echo "Found $location contains $category_name"."<br />";
-//                return true;
-//            }
-//            
-//            // 有时用户位置只有省，没有市（使用GPRS等网络时），因此还需要比较
-//            // 地方台所在省份，若包含省份名称，则该省的地方台也能被搜索到
-//            // 即：该省的用户可以搜索到该省对应的地方台的节目
-//            $more_locations = getMultiLocationsByCategory($category_id);
-//            foreach ($more_locations as $more_location)
-//            {
-//                if (strpos($location, $more_location) !== FALSE)
-//                {
-////                    echo "Found $location contains $more_location"."<br />";
-//                    return true;
-//                }
-//            }
-//        }
-//        return false;
-//    }
-    
     function getSearchCategories($profile)
     {
         $result = array("cctv", "satellitetv", "hd");

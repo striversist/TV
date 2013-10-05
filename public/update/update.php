10adae8b2e8430261cc593e02ce79f2e4b9e04a6
striversist <striversist@qq.com>
2013-10-05 10:01:14 +0800

    【数据】调整updata.php中版本号，不再做测试之用

--- public/update/update.php
+++ public/update/update.php
@@ -3,9 +3,9 @@ header("Content-type: text/html; charset=utf8");
 
 echo <<< EOT
 <Updateinfo>
-    <VersionCode>2</VersionCode>
-    <VersionName>1.0.2</VersionName>
-    <Url>http://striversist.oicp.net:52719/projects/TV/public/update/apk/TVGuide_1.0.2.apk</Url>
+    <VersionCode>1</VersionCode>
+    <VersionName>1.0.0</VersionName>
+    <Url>http://striversist.oicp.net:52719/projects/TV/public/update/apk/TVGuide_1.0.0.apk</Url>
     <ChannelVersion>1</ChannelVersion>
 </Updateinfo>
 EOT;

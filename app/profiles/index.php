d5e9a090bfb2b43d7ee230e0fb9d7ea8389559aa
striversist <striversist@qq.com>
2013-10-11 23:29:24 +0800

    【功能】展示访问的channel detail数量，接下来需要链接到另一页面展示详细内容。

--- app/profiles/index.php
+++ app/profiles/index.php
@@ -35,8 +35,18 @@
         $login_record = $db->getLoginRecordByDate("$today");
         $daily_activity = count($login_record["NewUsers"]) + count($login_record["LoyalUsers"]);
         $daily_new_user = count($login_record["NewUsers"]);
+        
+        $channel_visit_all = $db->getChannelVisitRecords();
+        $channel_visit_today = $channel_visit_all["$today"];
+        $daily_channel_visit_count = 0;
+        foreach ($channel_visit_today as $channel_id => $channel)
+        {
+//            echo "channel: ".$channel_id." VisitTimes=".$channel["VisitTimes"]."<br />";
+            $daily_channel_visit_count += $channel["VisitTimes"];
+        }
     ?>
     <p><h1>日活跃度：<?php echo $daily_activity; ?> = <?php echo $daily_new_user; ?>（日新增）+ <?php echo $daily_activity - $daily_new_user; ?>（老用户）</h1></p>
+    <p><h1>今日节目访问量：<?php echo $daily_channel_visit_count; ?></h1></p>
     <tbody>
         <tr>
             <th width="100">用户</th>

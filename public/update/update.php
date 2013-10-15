89b69c6e8c0d7e3a16b4926bf2897d6d63bd088f
striversist <striversist@qq.com>
2013-10-15 20:41:03 +0800

    【终端升级】调整URL域名。配合终端升级: versionCode=3, versionName=1.0.2

--- public/update/update.php
+++ public/update/update.php
@@ -3,9 +3,9 @@ header("Content-type: text/html; charset=utf8");
 
 echo <<< EOT
 <Updateinfo>
-    <VersionCode>2</VersionCode>
-    <VersionName>1.0.1</VersionName>
-    <Url>http://bigeyecow.oicp.net:52719/projects/TV/public/update/apk/TVGuide_1.0.1.apk</Url>
+    <VersionCode>3</VersionCode>
+    <VersionName>1.0.2</VersionName>
+    <Url>http://bigeyecow.oicp.net:52719/projects/TV/public/update/apk/TVGuide_1.0.2.apk</Url>
     <ChannelVersion>1</ChannelVersion>
 </Updateinfo>
 EOT;

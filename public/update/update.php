5068012540104e7a506c66ce069972ae18381b92
striversist <striversist@qq.com>
2014-01-08 21:57:10 +0800

    【数据】统一更新新增分类“movie”

--- public/update/update.php
+++ public/update/update.php
@@ -6,7 +6,7 @@ echo <<< EOT
     <VersionCode>8</VersionCode>
     <VersionName>1.2.2</VersionName>
     <Url>http://bigeyecow.oicp.net:52719/projects/TV/public/update/apk/TVGuide_1.2.2.apk</Url>
-    <ChannelVersion>2</ChannelVersion>
+    <ChannelVersion>3</ChannelVersion>
 </Updateinfo>
 EOT;
 

b3bfac6be85724be6547127bc2ac778b58be6a31
striversist <striversist@qq.com>
2014-01-13 23:39:54 +0800

    【数据】增加ChannelVersion版本号：新增频道CCTV5+、CCTV6（高清）、FOX

--- public/update/update.php
+++ public/update/update.php
@@ -6,7 +6,7 @@ echo <<< EOT
     <VersionCode>8</VersionCode>
     <VersionName>1.2.2</VersionName>
     <Url>http://bigeyecow.oicp.net:52719/projects/TV/public/update/apk/TVGuide_1.2.2.apk</Url>
-    <ChannelVersion>3</ChannelVersion>
+    <ChannelVersion>4</ChannelVersion>
 </Updateinfo>
 EOT;
 

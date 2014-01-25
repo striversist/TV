2d1140175570c48a2cafce3f00664a09f004f17d
striversist <striversist@qq.com>
2014-01-25 23:37:49 +0800

    【终端升级】versionCode=12, versionName=1.3.3

--- public/update/update.php
+++ public/update/update.php
@@ -3,9 +3,9 @@ header("Content-type: text/html; charset=utf8");
 
 echo <<< EOT
 <Updateinfo>
-    <VersionCode>8</VersionCode>
-    <VersionName>1.2.2</VersionName>
-    <Url>http://bigeyecow.oicp.net:52719/projects/TV/public/update/apk/TVGuide_1.2.2.apk</Url>
+    <VersionCode>12</VersionCode>
+    <VersionName>1.3.3</VersionName>
+    <Url>http://bigeyecow.oicp.net:52719/projects/TV/public/update/apk/TVGuide_1.3.3.apk</Url>
     <ChannelVersion>4</ChannelVersion>
 </Updateinfo>
 EOT;

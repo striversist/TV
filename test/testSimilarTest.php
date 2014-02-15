1a8dd23085e892ddf73d02a2e9dadb4587375253
striversist <striversist@qq.com>
2014-02-15 21:50:26 +0800

    【功能】从高频搜索词中过滤出有用词（即可能搜索出结果的词）

--- test/testSimilarTest.php
+++ test/testSimilarTest.php
@@ -1,11 +1,17 @@
 <?php
     header("Content-type: text/html; charset=utf8");
     require_once dirname(__FILE__).'/'.'../Database.php';
+    require_once dirname(__FILE__).'/'.'../public/json/utils.php';
     
     $db = Database::getInstance();
-    $searches = $db->getSearchWordsInDays(3);
-    $high = getHighFrequencyWords($searches, 6);
-    var_dump($high);
+    $searches = $db->getSearchWordsInDays(8);
+    $high_words = getHighFrequencyWords($searches, 20);
+    var_dump($high_words);
+   
+    foreach ($high_words as $keyword) 
+    {
+        echo "$keyword is availabel? => ".isAvailableSearchWord($keyword)."<br/>";
+    }
         
     // ------------------ Functions ------------------------
     function getHighFrequencyWords($array, $num)
@@ -35,12 +41,49 @@
             {
                 if (count($result) >= $num)
                     break;
-                if (!in_array($words[$i], $result))
+                if (!in_arrayi($words[$i], $result))
                     $result[] = $words[$i];
             }
         }
         
         return $result;
+    }
+    
+    function isAvailableSearchWord($keyword)
+    {
+        $channels = getMatchedChannels($keyword);
+        if (count($channels) > 0)
+            return true;
+        
+        $today = date("w") == "0" ? "7" : date("w");
+        $search_categories = getRootSearchCategories();
+        $programs = getMatchedPrograms($keyword, $search_categories, $today);
+        if (count($programs) > 0)
+            return true;
+        
+        return false;
+    }
+    
+    function getRootSearchCategories()
+    {
+        $collector = Collector::getInstance();
+        // 不想被搜索的分类：local，直接搜索耗时大，且其它省份无法看
+        $exclude_root_categories = array("local");
+        
+        $root_categories = $collector->getRootCategories();
+        $result = array();
+        foreach ($root_categories as $category_id => $category) 
+        {
+            if (!in_array($category_id, $exclude_root_categories))
+                $result[] = $category_id;
+        }
+        return $result;
+    }
+    
+    // case-insensitive in_array
+    function in_arrayi($needle, $haystack) 
+    {
+        return in_array(strtolower($needle), array_map('strtolower', $haystack));
     }
 
 ?>

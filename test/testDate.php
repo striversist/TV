03285b03bcbfc17d3d2cef5ac85ed0d8c589b95b
striversist <striversist@qq.com>
2014-02-17 21:08:26 +0800

    【测试】获取前一天的方法

--- test/testDate.php
+++ test/testDate.php
@@ -5,4 +5,7 @@
     echo date("H:i:s")."<br />";
     
     echo date("Y/m/d H:i:s")."<br />";
+    
+    $today = date("Y/m/d");
+    echo "today=$today, yestoday=".(date("Y/m/d",strtotime("-1 day")))."<br/>";
 ?>

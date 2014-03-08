e5f215bc8f93a2b540e4038ebf15a29329966aa3
striversist <striversist@qq.com>
2014-03-08 22:12:12 +0800

    【展示】周活跃度

--- app/profiles/version.php
+++ app/profiles/version.php
@@ -5,26 +5,45 @@
     $db = Database::getInstance();
     $profiles = $db->getProfiles();
     
-    $threshold_day = date("Y/m/d",strtotime("-30 day"));
-    $versions = array();
-    $total = 0;
+    $week_ago = date("Y/m/d",strtotime("-7 day"));
+    $month_ago = date("Y/m/d",strtotime("-30 day"));
+    
+    $versions_week = array();
+    $versions_month = array();
+    
+    $total_week = 0;
+    $total_month = 0;
     foreach ($profiles as $profile) 
     {
         if (isset($profile["Version"]))
         {
             $last_login = date("Y/m/d", strtotime($profile["LastLogin"]));
-            if ($last_login >= $threshold_day)
+            if ($last_login >= $week_ago)
             {
                 $version = $profile["Version"];
-                @$versions["$version"] += 1;
-                $total++;
+                @$versions_week["$version"] += 1;
+                $total_week++;
+            }
+            if ($last_login >= $month_ago)
+            {
+                $version = $profile["Version"];
+                @$versions_month["$version"] += 1;
+                $total_month++;
             }
         }
     }
-    krsort($versions);
+    krsort($versions_week);
+    krsort($versions_month);
     
-    echo "月活跃度： ".$total."<br/><br/>";
-    foreach ($versions as $version => $num) 
+    echo "周活跃度： ".$total_week."<br/><br/>";
+    foreach ($versions_week as $version => $num) 
+    {
+        echo "Version: $version =>  $num"."<br/>";
+    }
+    echo "<br/><br/>";
+    
+    echo "月活跃度： ".$total_month."<br/><br/>";
+    foreach ($versions_month as $version => $num) 
     {
         echo "Version: $version =>  $num"."<br/>";
     }

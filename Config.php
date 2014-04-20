937ea92c8248d7090a02507e1ec8c21c8a1196f0
striversist <striversist@qq.com>
2014-04-20 17:31:22 +0800

    【功能】将MAX_COLLECT_DAYS改为7天，从此不再从后台收集tvmao节目信息，交给终端收集。（tvmao会查IP，大量收集url会过滤得不到18:00以后节目，影响之后终端的开发）

--- Config.php
+++ Config.php
@@ -8,7 +8,7 @@ class Config
 {
     const DATA_SRC_TVMAO = "tvmao";
     const DATA_SRC_TVSOU = "tvsou";
-    const MAX_COLLECT_DAYS = 10;
+    const MAX_COLLECT_DAYS = 7;
     public static $DATA_SRC = self::DATA_SRC_TVSOU;
     public static $ChannelDetailFromWeb = true;
     public static $EnableAd = true;
